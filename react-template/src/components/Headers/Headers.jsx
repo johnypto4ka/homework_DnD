2656c74771bbd06a4be6a886dad63ea8c9579baf
Maksim-Attsetski <maks2001maks2001@gmail.com>
2022-03-09 12:12:03 +0300

    Add

---
+++ react-template/src/components/Headers/Headers.jsx
@@ -0,0 +1,10 @@
+import './Headers.css'
+
+function Headers () {
+    return (
+        <div>
+            Not imported, only example 
+        </div>
+    )
+}
+export default Headers;
---
+++ webpack-template/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  watchOptions: {
+    ignored: '**/node_modules',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+};
